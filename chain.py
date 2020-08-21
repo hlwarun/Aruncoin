9cafb18a1bb13ae24156a102ff2abefc17afb8fa
hlwarun <hlwarun@gmail.com>
2020-08-21 15:33:27 +0545

    Chain of Blocks

---
+++ chain.py
@@ -0,0 +1,36 @@
+class Blockchain():
+    # Produce hash of each block with 4 zeros in the beginning
+    difficulty = 4
+
+    def __init__(self, chain=[]):
+        self.chain = chain
+
+    def add_block(self, block):
+        self.chain.append(block)
+
+    def remove_block(self, block):
+        self.chain.remove(block)
+
+    def mining(self, block):
+        try:
+            block.hash_previous = self.chain[-1].hash()
+        except IndexError:
+            # THis will set the value of hash_previous = 000000... as we previously defined
+            pass
+        while True:
+            if block.hash()[:self.difficulty] == "0"*self.difficulty:
+                self.add_block(block)
+                break
+            else:
+                block.nonce += 1
+
+    def isValid(self):
+        for i in range(1, len(self.chain)):
+            _previous = self.chain[i].hash_previous
+            # Create a new hash for previous block
+            _current = self.chain[i-1].hash()
+            # Return INVALID if both hashesh do not match and if it does not contain declared number of zeros in the beiginning
+            if _previous != _current or _current[:self.difficulty] != "0"*self.difficulty:
+                return False
+
+        return True
